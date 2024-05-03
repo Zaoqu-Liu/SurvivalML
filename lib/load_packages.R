d877dd88c0f46200ad365f9440776fb33d2c8bdc
Liuzaoqu <68080738+Zaoqu-Liu@users.noreply.github.com>
2024-05-03 19:39:48 +0800

    Update load_packages.R

--- lib/load_packages.R
+++ lib/load_packages.R
@@ -1,62 +1,58 @@
 ##----------------------------------------------------------------------
 ## This script is designed for loading R packages in the main shiny app
 ##----------------------------------------------------------------------
-## Maintainer: Mingjie Wang (huzai920621@126.com)
-## Log: 
-## 2022-02-04, initial build
-
 
 #### Set packages resources ####
 options(BioC_mirror="https://mirrors.tuna.tsinghua.edu.cn/bioconductor")
-options(repos=structure(c(CRAN="https://mirrors.tuna.tsinghua.edu.cn/CRAN/")))  
+options(repos=structure(c(CRAN="https://mirrors.tuna.tsinghua.edu.cn/CRAN/")))
 
 message("[++] It will cost a while for the first time.")
 
 #### Load basic packages ####
-# if (!requireNamespace("pacman")) {
-#   install.packages("pacman")
-# }
-# library(pacman)
-# 
-# if (!requireNamespace("devtools")) {
-#   install.packages("devtools")
-# }
-# library(devtools)
+if (!requireNamespace("pacman")) {
+  install.packages("pacman")
+}
+library(pacman)
+
+if (!requireNamespace("devtools")) {
+  install.packages("devtools")
+}
+library(devtools)
 
 #### Load necessary packages ####
 ## Packages from Github
-# ## InteractiveComplexHeatmap and ComplexHeatmap packages MUST be installed from Github to use the latest version
-# if (!requireNamespace("ComplexHeatmap")) {
-#   message("[++] Installing ComplexHeatmap")
-#   tryCatch(
-#     devtools::install_github("jokergoo/ComplexHeatmap"),
-#     error = function(e) {
-#       message("[++] ERROR: R packages [ComplexHeatmap] from Github can NOT be installed.")
-#     }
-#   )
-# }
-# library(ComplexHeatmap)
+## InteractiveComplexHeatmap and ComplexHeatmap packages MUST be installed from Github to use the latest version
+if (!requireNamespace("ComplexHeatmap")) {
+  message("[++] Installing ComplexHeatmap")
+  tryCatch(
+    devtools::install_github("jokergoo/ComplexHeatmap"),
+    error = function(e) {
+      message("[++] ERROR: R packages [ComplexHeatmap] from Github can NOT be installed.")
+    }
+  )
+}
+library(ComplexHeatmap)
 
-# if (!requireNamespace("summaryBox")) {
-#   message("[++] Installing summaryBox")
-#   tryCatch(
-#     devtools::install_github("deepanshu88/summaryBox"),
-#     error = function(e) {
-#       message("[++] ERROR: R packages [summaryBox] from Github can NOT be installed.")
-#     }
-#   )
-# }
+if (!requireNamespace("summaryBox")) {
+  message("[++] Installing summaryBox")
+  tryCatch(
+    devtools::install_github("deepanshu88/summaryBox"),
+    error = function(e) {
+      message("[++] ERROR: R packages [summaryBox] from Github can NOT be installed.")
+    }
+  )
+}
 library(summaryBox)
 
-# if (!requireNamespace("bsplus")) {
-#   message("[++] Installing bsplus")
-#   tryCatch(
-#     devtools::install_github("ijlyttle/bsplus"),
-#     error = function(e) {
-#       message("[++] ERROR: R packages [bsplus] from Github can NOT be installed.")
-#     }
-#   )
-# }
+if (!requireNamespace("bsplus")) {
+  message("[++] Installing bsplus")
+  tryCatch(
+    devtools::install_github("ijlyttle/bsplus"),
+    error = function(e) {
+      message("[++] ERROR: R packages [bsplus] from Github can NOT be installed.")
+    }
+  )
+}
 library(bsplus)
 
 # Packages from cran and bioconductor
@@ -88,7 +84,9 @@ pkgs.to.check <- c(
   "RColorBrewer",
   "ggpubr",
   "BiocParallel",
-  "shinypop"
+  "shinypop",
+  'remotes'
+
 )
 
 for (pkg.name in pkgs.to.check) {
@@ -105,30 +103,30 @@ for (pkg.name in pkgs.to.check) {
 }
 
 # # packages to install
-# devtools::install_github("Github-Yilei/ggcor")
-# pacman::p_load("circlize")
-# pacman::p_load("ggridges")
-# pacman::p_load("RobustRankAggreg")
-# pacman::p_load("ggpubr")
-# pacman::p_load("org.Hs.eg.db")
-# pacman::p_load("GSVA")
-# pacman::p_load("survminer")
-# remotes::install_github("YuLab-SMU/clusterProfiler")
-# pacman::p_load("pROC")
-# pacman::p_load("WGCNA")
-# pacman::p_load("randomForestSRC")
-# pacman::p_load("glmnet")
-# pacman::p_load("gbm")
-# pacman::p_load("survivalsvm")
-# pacman::p_load("plsRcox")
-# pacman::p_load("RSpectra")
-# pacman::p_load("rARPACK")
-# pacman::p_load("mixOmics")
-# pacman::p_load("survcomp")
-# pacman::p_load("superpc")
-# devtools::install_github("binderh/CoxBoost")
-# pacman::p_load("timeROC")
-# pacman::p_load("rms")
-# pacman::p_load("dcurves")
-# pacman::p_load("snowfall")
+devtools::install_github("Github-Yilei/ggcor")
+pacman::p_load("circlize")
+pacman::p_load("ggridges")
+pacman::p_load("RobustRankAggreg")
+pacman::p_load("ggpubr")
+pacman::p_load("org.Hs.eg.db")
+pacman::p_load("GSVA")
+pacman::p_load("survminer")
+remotes::install_github("YuLab-SMU/clusterProfiler")
+pacman::p_load("pROC")
+pacman::p_load("WGCNA")
+pacman::p_load("randomForestSRC")
+pacman::p_load("glmnet")
+pacman::p_load("gbm")
+pacman::p_load("survivalsvm")
+pacman::p_load("plsRcox")
+pacman::p_load("RSpectra")
+pacman::p_load("rARPACK")
+pacman::p_load("mixOmics")
+pacman::p_load("survcomp")
+pacman::p_load("superpc")
+devtools::install_github("binderh/CoxBoost")
+pacman::p_load("timeROC")
+pacman::p_load("rms")
+pacman::p_load("dcurves")
+pacman::p_load("snowfall")
 
